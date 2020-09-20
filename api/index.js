1fca70bff3829c86e6f1a938372addbc2e70271a
Juliana Amoasei <juliana.amoasei@gmail.com>
2020-09-20 02:43:05 -0300

    video 1.4

--- api/index.js
+++ api/index.js
@@ -1,3 +1,23 @@
-const { ApolloServer } = require('apollo-server')
+const { ApolloServer, gql } = require('apollo-server')
 
-const server = new ApolloServer()
+const users = [
+  {
+    nome: "Ana",
+    ativo: true
+  },
+  {
+    nome: "Marcia",
+    ativo: false
+  }
+]
+
+const typeDefs = gql `
+  type User {
+    nome: String!
+    ativo: Boolean!
+    email: String
+  }
+`
+
+
+const server = new ApolloServer( { typeDefs, resolvers } )
