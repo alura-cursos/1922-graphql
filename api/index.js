c4aa8badccb3b0398c8365ce3f4fad189f934b28
Juliana Amoasei <juliana.amoasei@gmail.com>
2020-09-22 16:25:15 -0300

    video 2.3

--- api/index.js
+++ api/index.js
@@ -1,23 +1,12 @@
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer } = require('apollo-server')
+const userSchema = require('./user/schema/user.graphql')
+const userResolver = require('./user/resolvers/userResolvers')
 
-const users = [
-  {
-    nome: "Ana",
-    ativo: true
-  },
-  {
-    nome: "Marcia",
-    ativo: false
-  }
-]
-
-const typeDefs = gql `
-  type User {
-    nome: String!
-    ativo: Boolean!
-    email: String
-  }
-`
-
+const typeDefs = [userSchema]
+const resolvers = [userResolver]
 
 const server = new ApolloServer( { typeDefs, resolvers } )
+
+server.listen().then(({url}) => {
+  console.log(`Servidor rodando na porta ${url}`)
+})
