18ac9b008cd466a42d94115013e0318c827e5f20
Juliana Amoasei <juliana.amoasei@gmail.com>
2020-09-23 23:31:16 -0300

    video 3.1

--- api/index.js
+++ api/index.js
@@ -1,11 +1,20 @@
 const { ApolloServer } = require('apollo-server')
 const userSchema = require('./user/schema/user.graphql')
 const userResolvers = require('./user/resolvers/userResolvers')
+const UsersAPI = require('./user/datasource/user')
 
 const typeDefs = [userSchema]
 const resolvers = [userResolvers]
 
-const server = new ApolloServer( { typeDefs, resolvers } )
+const server = new ApolloServer( { 
+  typeDefs,
+  resolvers,
+  dataSources: () => {
+    return {
+      usersAPI: new UsersAPI()
+    }
+  }
+} )
 
 server.listen().then(({url}) => {
   console.log(`Servidor rodando na porta ${url}`)
