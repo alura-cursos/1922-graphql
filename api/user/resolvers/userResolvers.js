e439b65d1254fde60c66e285289304904df74ff9
Juliana Amoasei <juliana.amoasei@gmail.com>
2020-09-24 21:52:34 -0300

    video 4.1

--- api/user/resolvers/userResolvers.js
+++ api/user/resolvers/userResolvers.js
@@ -2,7 +2,10 @@ const userResolvers = {
   Query: {
     users: (root, args, { dataSources }) => dataSources.usersAPI.getUsers(),
     user: (root, { id }, { dataSources }) => dataSources.usersAPI.getUserById(id)
-  }
+  },
+  Mutation: {
+    adicionaUser: async (root, user, { dataSources }) => dataSources.usersAPI.adicionaUser(user),
+  } 
 }
 
 module.exports = userResolvers
